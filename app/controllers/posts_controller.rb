9443c36b0f34c1059381d7d24b973f9e6b8e7079
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-17 17:42:10 +0900

    second commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  # def show
+  #   @post = Post.find(params[:id])
+  # end
+
+  # def edit
+  #   @post = Post.find(params[:id])
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
+  end
+
+
+end
