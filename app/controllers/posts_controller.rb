d8e2f97cbaafa5d8f0c74441722b5f58a885c6ad
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-18 18:38:08 +0900

    deviseの導入＆設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,8 +32,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :text, :image)
+    params.require(:post).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
-
-
 end
