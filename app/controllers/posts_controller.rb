cc0798331185314fdc7af4c6b0608541735f415c
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-21 22:42:07 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order(created_at: :desc)
   end
 
   def new
