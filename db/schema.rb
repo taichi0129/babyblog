534c4c7c97f0340ec65ccec23170b28f5f94417c
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-24 18:45:57 +0900

    ユーザー画像機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_013805) do
+ActiveRecord::Schema.define(version: 2020_06_24_031324) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_013805) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
