4b0114722f76fc34eb68773266c1030a05afa9a7
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-30 19:33:38 +0900

    [WIP]ユーザー画像登録と情報編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @user = User.find(current_user.id)
   end
+
   def new
   end
   def create
   end
+
   def show
-    @user = User.find(params[:id])
   end
+
   def edit
   end
+
   def update
+    @user.update(user_params)
+    redirect_to users_path(@user.id)
   end
+
   def destroy
+    if @user.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :image)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
