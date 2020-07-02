53cdac60819d54fa82a57b0c2329949b8694967a
taichi0129 <nishi.made.t.9210@gmail.com>
2020-07-02 19:43:05 +0900

    モデル内容追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :likes
-  has_many :relationships
+  has_many :likes, dependent: :destroy
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
