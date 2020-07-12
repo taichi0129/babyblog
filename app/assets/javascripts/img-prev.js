615c7e7ceccad162f6c0da11835cea39831ceb1b
taichi0129 <nishi.made.t.9210@gmail.com>
2020-07-12 23:18:34 +0900

    [WIP]ファイルプレビュー表示

--- app/assets/javascripts/img-prev.js
+++ app/assets/javascripts/img-prev.js
@@ -7,8 +7,10 @@ $(function() {
       var src = fileReader.result;
       var html = `<div class="image-form">
                     <img src="${src}" class="image-data">
-                  </div>`;
+                  </div>
+                  <p><a href="#" class="img_del">画像を削除する</a></p>`;
       $('.hidden-image').after(html);
+      $('.hidden-image').css('display', 'none');
     }
     fileReader.readAsDataURL(file);
     if ($('.preview-image').length != 0) {
@@ -16,4 +18,17 @@ $(function() {
       $('.preview-image .image-data').attr({ src: src });
     }
   });
+  function img_del(target){
+    target.find("a.img_del").on('click',function(){
+      var self = $(this),
+          parentBox = self.parent().parent().parent();
+      if(window.confirm('画像を削除します。\nよろしいですか？')){
+        setTimeout(function(){
+          parentBox.find('input[type=file]').val('');
+          parentBox.find('.image-form').remove();
+        } , 0);
+      }
+      return false;
+    });
+  }
 });
