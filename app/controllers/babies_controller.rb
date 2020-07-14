edc1887dc9a13a0bcf3e8b12c8a0842663ca261f
taichi0129 <nishi.made.t.9210@gmail.com>
2020-07-14 19:38:07 +0900

    マイベビーedit,update追加

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -1,4 +1,6 @@
 class BabiesController < ApplicationController
+  before_action :set_baby, only: [:edit, :update]
+
   def new
     @baby = Baby.new
   end
@@ -12,8 +14,21 @@ class BabiesController < ApplicationController
     end
   end
 
+  def edit
+    @baby = Baby.find(params[:id])
+  end
+
+  def update
+    @baby.update(baby_params)
+    redirect_to user_path(current_user.id)
+  end
+
   private
   def baby_params
     params.require(:baby).permit(:name, :sex, :image, :birthday, :first_weight, :first_height).merge(user_id: current_user.id)
+  end
+
+  def set_baby
+    @baby = Baby.find(params[:id])
   end
 end
